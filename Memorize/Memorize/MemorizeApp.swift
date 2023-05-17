8969a4a398dfc73ae741c501d6604b358c52692a
Rafael Melo <rafaelnunesmelo@gmail.com>
2023-05-17 14:17:43 -0300

    Criando projeto

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rafael Melo on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
