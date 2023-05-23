fa584bb60e0dd1e1b47f1d13de14f28c7f8149e0
Rafael Melo <rafaelnunesmelo@gmail.com>
2023-05-23 17:02:36 -0300

    Uso de geometricReader para aproveitar espaço

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -36,24 +36,35 @@ struct CardView: View {
     }
     
     var body: some View {
-        ZStack {
-            //Iniciar com borda arredondada
-            let shape = RoundedRectangle(cornerRadius: 20)
-            if card.isFaceUp {
-                // Preencher fundo com cor branca
-                shape.fill().foregroundColor(.white)
-                // Retangulo com borda apenas
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content).font(.largeTitle)
-            } else if card.isMatched {
-                shape.opacity(0)
-            } else {
-                // Preencher background da cor padrão
-                shape.fill()
+        GeometryReader { geometry in
+            ZStack {
+                //Iniciar com borda arredondada
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                if card.isFaceUp {
+                    // Preencher fundo com cor branca
+                    shape.fill().foregroundColor(.white)
+                    // Retangulo com borda apenas
+                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Text(card.content).font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else {
+                    // Preencher background da cor padrão
+                    shape.fill()
+                }
             }
         }
     }
     
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    }
 
 }
 
