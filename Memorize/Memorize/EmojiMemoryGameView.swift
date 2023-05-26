b18fece0c8d9815c1fd149ca95b0da10fc7460b9
Rafael Melo <rafaelnunesmelo@gmail.com>
2023-05-26 17:03:38 -0300

    Criando view de deck e animação de cards saindo dele

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -10,23 +10,55 @@ import SwiftUI
 struct EmojiMemoryGameView: View {
     @ObservedObject var game: EmojiMemoryGame
     
+    @Namespace private var dealingNamespace
+    
     var body: some View {
         VStack {
             gameBody
+            deckBody
             shuffle
         }
         //Espaçamento das margens
         .padding()
     }
     
+    @State private var dealt = Set<Int>()
+    
+    private func deal(_ card: EmojiMemoryGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: EmojiMemoryGame.Card) -> Bool{
+        !dealt.contains(card.id)
+    }
+    //calcular delay da animação  para sair cada card por vez do deck
+    private func dealAnimation(for card: EmojiMemoryGame.Card) -> Animation {
+        var delay = 0.0
+        if let index = game.cards.firstIndex(where: { $0.id == card.id }) {
+            delay = Double(index) * (CardConstants.totalDealDuration / Double(game.cards.count))
+        }
+        return Animation.easeInOut(duration: CardConstants.dealDuration).delay(delay)
+    }
+    
+    private func zIndex(of card: EmojiMemoryGame.Card) -> Double {
+        -Double(game.cards.firstIndex(where: { $0.id == card.id }) ?? 0)
+    }
+    
     var gameBody: some View {
-        AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            if card.isMatched && !card.isFaceUp {
+        AspectVGrid(items: game.cards, aspectRatio: CardConstants.aspectRatio) { card in
+            if isUndealt(card) || card.isMatched && !card.isFaceUp {
                 //Esconder a view
                 Color.clear
             } else {
                 CardView(card)
+                    //fazer animação conjunta com o deck
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                     .padding(4)
+                    //transição ao sumir e aparecer
+                    .transition(AnyTransition.asymmetric(insertion: .identity, removal: .scale))
+                    //Ajustar posição dos card no deck
+                    .zIndex(zIndex(of: card))
+                    //ação de toque
                     .onTapGesture {
                         withAnimation {
                             game.choose(card)
@@ -35,7 +67,28 @@ struct EmojiMemoryGameView: View {
             }
         }
         //Cor de tudo dentro da view
-        .foregroundColor(.red)
+        .foregroundColor(CardConstants.redColor)
+    }
+    
+    var deckBody: some View {
+        ZStack {
+            ForEach(game.cards.filter(isUndealt)) { card in
+                CardView(card)
+                    //fazer animação conjunta com o deck
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(AnyTransition.asymmetric(insertion: .opacity, removal: .identity))
+                    .zIndex(zIndex(of: card))
+            }
+        }
+        .frame(width: CardConstants.undealtWidth, height: CardConstants.undealtHeight)
+        .foregroundColor(CardConstants.redColor)
+        .onTapGesture {
+            for card in game.cards {
+                withAnimation(dealAnimation(for: card)) {
+                    deal(card)
+                }
+            }
+        }
     }
     
     var shuffle: some View {
@@ -44,6 +97,15 @@ struct EmojiMemoryGameView: View {
                 game.shuffle()
             }
         }
+    }
+    
+    private struct CardConstants {
+        static let redColor: Color = .red
+        static let aspectRatio: CGFloat = 2/3
+        static let dealDuration: Double = 0.5
+        static let totalDealDuration: Double = 2
+        static let undealtHeight: CGFloat = 90
+        static let undealtWidth = undealtHeight * aspectRatio
     }
 }
 
